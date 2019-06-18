1937bc8cf2a862dd25aca9a0f43be042bf1bdfe1
JoeBumbaca <joebumbaca@gmail.com>
2019-06-18 16:35:10 -0700

    Fix buy and add winner text

--- tic_tac_toe_project/tic_tac_toe_js/src/ttt-view.js
+++ tic_tac_toe_project/tic_tac_toe_js/src/ttt-view.js
@@ -18,21 +18,30 @@ class View {
   makeMove($square) {
   
     let $pos = $square.data('pos');
-    
-    // debugger
-    if (this.game.currentPlayer === 'x'){
-      $square.removeClass();
-      $square.addClass('x');
-      $square.text('X');
-    }else{
-      $square.removeClass();
-      $square.addClass('o');
-      $square.text('O');
-    }
-    this.game.playMove($pos);
+    let $class = $square.attr('class');
 
-    if(this.game.winner()){
-      alert(`The winner is ${this.game.winner()}!`);
+    if($class === 'grey'){
+    
+      if (this.game.currentPlayer === 'x'){
+        $square.removeClass();
+        $square.addClass('x');
+        $square.text('X');
+      }else{
+        $square.removeClass();
+        $square.addClass('o');
+        $square.text('O');
+      }
+
+      this.game.playMove($pos);
+      
+
+      if(this.game.winner()){
+        // alert(`The winner is ${this.game.winner()}!`);
+        let $h3 = $('h3');
+        $h3.text(`The winner is ${this.game.winner()}!`);
+      }
+    }else{
+      alert('Invalid');
     }
   }
 
